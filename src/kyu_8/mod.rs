b30ec441c938823dae1a347a284af1859eebdf80
Eduardo Russo <45946593+ddrdev@users.noreply.github.com>
2024-12-20 18:27:44 -0300

    (8 kyu) Get Nth Even Number

--- src/kyu_8/mod.rs
+++ src/kyu_8/mod.rs
@@ -1,7 +1,9 @@
 mod plural;
 mod symmetric_point;
 mod cannons_ready;
+mod nth_even;
 
 pub use plural::plural;
 pub use symmetric_point::symmetric_point;
 pub use cannons_ready::cannons_ready;
+pub use nth_even::nth_even;
---
+++ src/kyu_8/nth_even.rs
@@ -0,0 +1,3 @@
+pub fn nth_even(n: u32) -> u32 {
+    2 * (n-1)
+}
